f9c7b4741c0cbe4c05223d14329d5d49c7e4d4d0
Student User <student@workstation.lab.example.com>
2020-12-17 14:04:10 -0500

    changes

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "This train is for Cockfosters!\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "This train is for Cockfosters!\n";
 ?>
